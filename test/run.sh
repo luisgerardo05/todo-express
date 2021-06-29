1add707aee3ae62004dfe811367a6982caef0f4b
Luis Huerta <luisgerhuerta@gmail.com>
2021-06-29 14:58:44 -0500

    Added commands to run each file

--- test/run.sh
+++ test/run.sh
@@ -1,1 +1,9 @@
-locust -f locustfile.py --host http://localhost.com:80 --headless -u 1 -r 1 --run-time 1m --stop-timeout 30
+#!/bin/bash
+
+locust -f control.py --host http://localhost.com:80 --headless -u 5 -r 5 --run-time 10s --stop-timeout 10 --exit-code-on-error 1
+
+locust -f create.py --host http://localhost.com:80 --headless -u 5 -r 5 --run-time 10s --stop-timeout 10 --exit-code-on-error 1
+
+locust -f update.py --host http://localhost.com:80 --headless -u 5 -r 5 --run-time 10s --stop-timeout 10 --exit-code-on-error 1
+
+locust -f delete.py --host http://localhost.com:80 --headless -u 5 -r 5 --run-time 10s --stop-timeout 10 --exit-code-on-error 1
